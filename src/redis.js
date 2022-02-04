73909cdbb5b89ff69f9f862c6578e0bfff5d44cf
Ty Chermsirivatana <professornitro@gmail.com>
2022-02-04 07:44:50 -0500

    Update redis.js

--- src/redis.js
+++ src/redis.js
@@ -1,4 +1,29 @@
 import Redis from "ioredis";
 import credentials from "./credentials.js";
 
-export const redisClient = new Redis(credentials.redisUrl);
+export const redisClient = new Redis.Cluster([
+  {
+    port: 6379,
+    host: "10.42.0.4",
+  },
+  {
+    port: 6379,
+    host: "10.42.0.5",
+  },
+  {
+    port: 6379,
+    host: "10.42.0.6",
+  },
+  {
+    port: 6379,
+    host: "10.42.0.7",
+  }, 
+  {
+    port: 6379,
+    host: "10.42.0.9",
+  },
+  {
+    port: 6379,
+    host: "10.42.0.12",
+  }, 
+]);
